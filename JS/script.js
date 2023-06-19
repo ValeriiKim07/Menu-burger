55bef8192e709260e44c1045e9cc5346186c9962
kim <keembeatsprod@gmail.com>
2023-06-19 13:34:10 +0300

    mouve to chapter

--- JS/script.js
+++ JS/script.js
@@ -44,3 +44,29 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
+
+
+// scroll links
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', oneMenuLinkClick);
+    });
+
+    function oneMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+        
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+
